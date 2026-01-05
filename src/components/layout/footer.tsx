d6ec50dd4d7d73511585d3455580eaab4088a550
zaylee-x <zilyasriva@gmail.com>
2026-01-05 10:49:44 +0700

    add link github

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -31,7 +31,7 @@ export function Footer() {
             target="_blank"
             className="hover:opacity-80 transition"
           >
-            <Image src="/Github.png" alt="GitHub" width={24} height={24} />
+            <Image src="/GitHub.png" alt="GitHub" width={24} height={24} />
           </Link>
           <Link
             href="www.linkedin.com/in/sylva-zilyasri"
